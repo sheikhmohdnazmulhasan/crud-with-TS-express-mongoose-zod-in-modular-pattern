ee567f2d9b34dc9d117cfea1560bffb99c976a6c
mdnazmul42726 <nazmulofficial@outlook.com>
2024-05-21 23:41:26 +0600

    mongoose connected successfully

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
+import mongoose from 'mongoose';
 import app from './app';
+import 'dotenv/config'
 
 (
     async function () {
 
-        app.listen(5000, () => {
-            console.log(`project-1 listening on port ${5000}`);
-        });
+        try {
+
+            await mongoose.connect(process.env.MONGODB_URL as string);
+            console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+            app.listen(5000, () => {
+                console.log(`project-1 listening on port ${5000}`);
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 )();
